470efa39fd9bb7748c9ba3f7e7e398b6dc3870e4
DavidClifford-Pro <bassapproved@gmail.com>
2021-01-15 14:41:23 -0500

    Added New Controller Information

--- src/com/project/BoardController/BoardController.java
+++ src/com/project/BoardController/BoardController.java
@@ -1,5 +1,6 @@
 package com.project.BoardController;
 
+import com.project.ChessPieces.IChessPiece;
 import com.project.TeamController.Team;
 import com.project.TeamController.TeamType;
 
@@ -19,5 +20,40 @@ public class BoardController {
 	
 	public Team getTeam2() {
 		return team2;
+	}
+	
+	
+	// Returns null if there is no piece at the location
+	public IChessPiece getPieceAtLocation(Location location) {
+		for(IChessPiece piece : getTeam1().getChessPieces()) {
+			if(piece.getLocation().getX() == location.getX() && piece.getLocation().getZ() == location.getZ()) {
+				return piece;
+			}
+		}
+		
+		for(IChessPiece piece : getTeam2().getChessPieces()) {
+			if(piece.getLocation().getX() == location.getX() && piece.getLocation().getZ() == location.getZ()) {
+				return piece;
+			}
+		}
+		return null;
+	}
+	
+	// Returns the team the piece belongs to
+	// Will return null if the piece somehow doesn't belong to a specific team
+	
+	public Team getTeamPieceBelongsTo(IChessPiece piece) {
+		
+		if(team1.getChessPieces().contains(piece)) {
+			return team1;
+		}
+		
+		if(team2.getChessPieces().contains(piece)) {
+			return team2;
+		}
+		
+		return null;
+		
+		
 	}
 }
