9d9ab513decdfb3d563213555fa1fb3fc915d972
DavidClifford-Pro <bassapproved@gmail.com>
2021-01-18 12:50:45 -0500

    AI Handler Update

--- src/com/project/AiController/AIController.java
+++ src/com/project/AiController/AIController.java
@@ -2,6 +2,7 @@ package com.project.AiController;
 
 import javax.swing.JFrame;
 
+import com.project.Main.Main;
 import com.project.TeamController.Team;
 
 // Object that handles the AI for the specified team
@@ -11,11 +12,14 @@ public class AIController {
 	
 	private JFrame frame;
 	
+	private boolean cancel;
+	
 	// Create a new AI controller for the specified team
 	public AIController(Team team, JFrame frame) {
 		this.team = team;
 		this.frame = frame;
-		startAI();
+		cancel = false;
+		runAIHandler();
 	}
 	
 	public Team getTeam() {
@@ -29,13 +33,32 @@ public class AIController {
 	
 	
 	// TODO write code here to manage AI movements
-	private void startAI() {
-		
+	private void runAI() {
+		if(Main.getBoardController().getCurrentPlayerToMove().equals(team.getTeamType())) {
+			// Run the code here
+		}
 	}
+	
+	private void runAIHandler() {
+		new java.util.Timer().schedule( 
+		        new java.util.TimerTask() {
+		            @Override
+		            public void run() {
+		            	if(cancel) return; // If the AI was canceled, don't run AI code
+		            	if(!getAIFrame().isActive() || !getAIFrame().isDisplayable()) return; // If the frame is over, cancel it
+		            	if(Main.getBoardController().hasGameEnded()) return; // Cancel if the game is over
+		            	runAI();
+		            	runAIHandler();
+		            }
+		            	
+		        }, 2500 // Run this every 2.5 seconds
+		);
+	}
+	
 	
 	// Cancels the AI from working
 	public void cancelAI() {
-		
+		cancel = true;
 	}
 	
 }
