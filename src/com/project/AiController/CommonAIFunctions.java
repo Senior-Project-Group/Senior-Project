7b1483798679336e09d2cdf5c61985992b0c92e4
DavidClifford-Pro <bassapproved@gmail.com>
2021-02-11 09:57:52 -0500

    Added To CommonAIFunctions

--- src/com/project/AiController/CommonAIFunctions.java
+++ src/com/project/AiController/CommonAIFunctions.java
@@ -2,7 +2,8 @@ package com.project.AiController;
 
 public class CommonAIFunctions {
 
-	public CommonAIFunctions() {}
-	
-	
+	private AIController controller;
+	public CommonAIFunctions(AIController controller) {
+		this.controller = controller;
+	}
 }
