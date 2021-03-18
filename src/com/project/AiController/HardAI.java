91566e375ace0ffef1c75320b675ebb0226b06e0
lmoore84 <48740428+lmoore84@users.noreply.github.com>
2021-03-18 15:09:50 -0400

    Update HardAI.java

--- src/com/project/AiController/HardAI.java
+++ src/com/project/AiController/HardAI.java
@@ -1,3 +1,4 @@
+//Alpha-beta pruning in the hardest difficulty will reduce noncritical moves in the search tree, making the AI more strategic.
 package com.project.AiController;
 
 import java.util.ArrayList;
