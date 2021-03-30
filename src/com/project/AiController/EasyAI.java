ec7bdd624b654be4556ae1c99d48c2b907559ba0
DavidClifford-Pro <bassapproved@gmail.com>
2021-03-30 09:28:52 -0400

    AI logic?

--- src/com/project/AiController/EasyAI.java
+++ src/com/project/AiController/EasyAI.java
@@ -19,6 +19,19 @@ public class EasyAI {
 	}
 	
 	private void run() {
+		// How this AI will work:
+		
+		// Most dangerous pieces: King, queen (has the second most power), Knight (knight has the most power)
+		// Goal: Kill king, if can't kill king, then kill bishop next, then knight then queen
+		
+		// Check if any piece can take out the king (if the chance is 50% or higher perform action)
+		// Check if any bishops can be taken out next (if the chance is 50% or higher perform action)
+		
+		// Check if king/bishops on our side are in danger.  If there is over a 33% chance, attempt to move piece to counter it
+		// If they aren't in danger, find all pieces that can die on the other team and try and kill it, but check if moving the piece will make the king/bishops exposed and at risk
+		
+		// If no pieces can kill a piece, then randomly move a piece, but check if moving the piece will make the king/bishops exposed and at risk
+		
 		ArrayList<PieceInformation> information = commonFunctionsController.getAllPossibleMoves();
 		boolean hasPerformedAction = false;
 		ProbabilityController prod = new ProbabilityController();
