91e48a390d51c968fecef58268f5e4731bd6f2e0
DavidClifford-Pro <bassapproved@gmail.com>
2021-04-19 19:19:45 -0400

    Fixes
    - Single player works now
    - Need to fix archer and knight special moves for single player

--- src/com/project/ChessPieces/DistanceCalculator.java
+++ src/com/project/ChessPieces/DistanceCalculator.java
@@ -11,29 +11,48 @@ public class DistanceCalculator {
 		
 	private ArrayList<Location> locations = new ArrayList<Location>();
 	
-	public ArrayList<Location> findPossiblePaths(IChessPiece piece, int checkAmount) {
+	int[][] offsets = {
+	        {1, 0},
+	        {0, 1},
+	        {-1, 0},
+	        {0, -1},
+	        {1, 1},
+	        {-1, 1},
+	        {-1, -1},
+	        {1, -1},
+	       		        
+	    };
+	
+	public ArrayList<Location> findPossiblePaths(IChessPiece startPiece, int checkAmount) {
 		locations.clear();
-		recersivePath(piece, piece.getLocation(), checkAmount, 0);
+
+		Location from = startPiece.getLocation();
+		
+		// Initial check for attacking pieces
+		for(int x = 0; x != 8; x++) {
+			int xOffset = offsets[x][0];
+			int zOffset = offsets[x][1];
+			Location loc = new Location(from.getX() + xOffset, from.getZ() + zOffset);
+			if(Main.getBoardController().isLocationOnBoard(loc)) {
+				IChessPiece piece = Main.getBoardController().getPieceAtLocation(loc);
+				if(piece != null) {
+					if(!piece.getTeamType().equals(startPiece.getTeamType())) {
+						addToList(startPiece.getLocation(), loc, checkAmount);
+					}
+				}
+			}
+		}
+		
+		recersivePath(startPiece, startPiece.getLocation(), checkAmount, 0);
 		return locations;
 	}
+	
 	
 	// On start the currentPathAmount = 0
 	private void recersivePath(IChessPiece startPiece, Location from, int checkAmount, int currentPathAmount) {
 		if(currentPathAmount >= checkAmount) {
 			return;
 		}
-		
-		int[][] offsets = {
-		        {1, 0},
-		        {0, 1},
-		        {-1, 0},
-		        {0, -1},
-		        {1, 1},
-		        {-1, 1},
-		        {-1, -1},
-		        {1, -1},
-		       		        
-		    };
 		
 		for(int x = 0; x != 8; x++) {
 			int xOffset = offsets[x][0];
@@ -44,8 +63,8 @@ public class DistanceCalculator {
 				if(piece != null) {
 					if(!piece.getTeamType().equals(startPiece.getTeamType())) {
 						// It's an enemy piece
-						addToList(startPiece.getLocation(), loc, checkAmount);
-						//recersivePath(startPiece, loc, checkAmount, (currentPathAmount + 1));
+						// addToList(startPiece.getLocation(), loc, checkAmount);
+						// recersivePath(startPiece, loc, checkAmount, (currentPathAmount + 1));
 					}
 				}else {
 					if(!loc.equals(from)) {
