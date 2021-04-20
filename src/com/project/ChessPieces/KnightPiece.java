0855e2e38163746420e529318fb5f18928e9c4e9
DavidClifford-Pro <bassapproved@gmail.com>
2021-04-20 09:34:54 -0400

    Special Moves
    - Archer is done
    - Working on knight

--- src/com/project/ChessPieces/KnightPiece.java
+++ src/com/project/ChessPieces/KnightPiece.java
@@ -19,10 +19,14 @@ public class KnightPiece implements IChessPiece{
 	
 	private Location startLocation;
 	
-	@SuppressWarnings("unused")
 	private TeamType team;
 	
+	private boolean canDoSpecialMove;
+	
+	private boolean movedFirst = false;
+	
 	public KnightPiece(Location location, TeamType team) {
+		this.canDoSpecialMove = false;
 		this.team = team;
 		isAlive = true;
 		hasMovedOnce = false;
@@ -84,11 +88,66 @@ public class KnightPiece implements IChessPiece{
 		return team;
 	}
 	
+	public boolean canDoSpecialMove() {
+		return canDoSpecialMove;
+	}
+	
+	public void allowSpecialMove() {
+		System.out.println("1");
+		this.canDoSpecialMove = true;
+		movedFirst = true;
+	}
+	
+	public void specialMovePerformed() {
+		System.out.println("2");
+		this.canDoSpecialMove = false;
+	}
+	
+	public boolean movedFirst() {
+		return movedFirst;
+	}
+	
+	public void resetMovedFirst() {
+		System.out.println("3");
+		movedFirst = false;
+	}
+	
 	@Override
 	public ArrayList<Location> getPossibleMoves() {
 		DistanceCalculator cal = new DistanceCalculator();
 
 		return cal.findPossiblePaths(this, 5);
+	}
+	
+	public ArrayList<Location> getAttackLocations() {
+		ArrayList<Location> attackLocations = new ArrayList<Location>();
+		int[][] offsets = {
+		        {1, 0},
+		        {0, 1},
+		        {-1, 0},
+		        {0, -1},
+		        {1, 1},
+		        {-1, 1},
+		        {-1, -1},
+		        {1, -1},
+		       		        
+		    };
+		
+		for(int x = 0; x != 8; x++) {
+			int xOffset = offsets[x][0];
+			int zOffset = offsets[x][1];
+			Location loc = new Location(this.getLocation().getX() + xOffset, this.getLocation().getZ() + zOffset);
+			if(Main.getBoardController().isLocationOnBoard(loc)) {
+				IChessPiece piece = Main.getBoardController().getPieceAtLocation(loc);
+				if(piece != null) {
+					if(!piece.getTeamType().equals(this.getTeamType())) {
+						attackLocations.add(loc);
+					}
+				}
+			}
+		}
+		return attackLocations;
+		
 	}
 	
 }
--- src/com/project/ChessPieces/RookPiece.java
+++ src/com/project/ChessPieces/RookPiece.java
@@ -115,11 +115,11 @@ public class RookPiece implements IChessPiece{
 			}
 		}
 		
-		ArrayList<Location> attackLocations = new DistanceCalculator().findPossiblePaths(this, 2);
+		ArrayList<Location> attackLocations = findPossiblePaths(this, 2);
 		for(Location attack : attackLocations) {
 			IChessPiece atLocation = Main.getBoardController().getPieceAtLocation(attack);
 			if(atLocation != null) {
-				if(!atLocation.getTeamType().equals(this.getTeamType())) {
+				if(!atLocation.getTeamType().equals(this.getTeamType()) && !locationsToMove.contains(attack)) {
 					locationsToMove.add(attack);
 				}
 				
@@ -130,6 +130,99 @@ public class RookPiece implements IChessPiece{
 		
 		
 		return locationsToMove;
+	}
+	
+	private ArrayList<Location> locations = new ArrayList<Location>();
+	
+	int[][] offsets = {
+	        {1, 0},
+	        {0, 1},
+	        {-1, 0},
+	        {0, -1},
+	        {1, 1},
+	        {-1, 1},
+	        {-1, -1},
+	        {1, -1},
+	       		        
+	    };
+	
+	
+	private ArrayList<Location> findPossiblePaths(IChessPiece startPiece, int checkAmount) {
+		locations.clear();
+
+		Location from = startPiece.getLocation();
+		
+		// Initial check for attacking pieces
+		for(int x = 0; x != 8; x++) {
+			int xOffset = offsets[x][0];
+			int zOffset = offsets[x][1];
+			Location loc = new Location(from.getX() + xOffset, from.getZ() + zOffset);
+			if(Main.getBoardController().isLocationOnBoard(loc)) {
+				IChessPiece piece = Main.getBoardController().getPieceAtLocation(loc);
+				if(piece != null) {
+					if(!piece.getTeamType().equals(startPiece.getTeamType())) {
+						addToList(startPiece.getLocation(), loc, checkAmount);
+					}
+				}
+			}
+		}
+		
+		recersivePath(startPiece, startPiece.getLocation(), checkAmount, 0);
+		return locations;
+	}
+	
+	
+	// On start the currentPathAmount = 0
+	private void recersivePath(IChessPiece startPiece, Location from, int checkAmount, int currentPathAmount) {
+		if(currentPathAmount >= checkAmount) {
+			return;
+		}
+		
+		for(int x = 0; x != 8; x++) {
+			int xOffset = offsets[x][0];
+			int zOffset = offsets[x][1];
+			Location loc = new Location(from.getX() + xOffset, from.getZ() + zOffset);
+			if(Main.getBoardController().isLocationOnBoard(loc)) {
+				IChessPiece piece = Main.getBoardController().getPieceAtLocation(loc);
+				if(piece != null) {
+					if(!piece.getTeamType().equals(startPiece.getTeamType())) {
+						// It's an enemy piece
+						 addToList(startPiece.getLocation(), loc, checkAmount);
+						// recersivePath(startPiece, loc, checkAmount, (currentPathAmount + 1));
+					}
+				}else {
+					if(!loc.equals(from)) {
+						addToList(startPiece.getLocation(), loc, checkAmount);
+						recersivePath(startPiece, loc, checkAmount, (currentPathAmount + 1));
+						
+					}
+				}
+			}
+		}
+	}
+	
+	// Return 1 is in range, return -1 if not
+	private int getDistance(Location from, Location to, int checkAmount) {
+		if(!Main.getBoardController().isLocationOnBoard(to) || !Main.getBoardController().isLocationOnBoard(from)) {
+			return -1;
+		}
+		
+		int xDistance = Math.abs(from.getX() - to.getX());
+		int zDistance = Math.abs(from.getZ() - to.getZ());
+		
+		if(xDistance <= checkAmount && zDistance <= checkAmount) {
+			return 1;
+		}
+		
+		return -1;
+	}
+	
+	private void addToList(Location from, Location to, int checkAmount) {
+		if(!locations.contains(to)) {
+			if(getDistance(from, to, checkAmount) == 1) {
+				locations.add(to);
+			}
+		}
 	}
 	
 }
