c525b2a473f4c510d6fdb249a71bc0a2c12ba929
DavidClifford-Pro <bassapproved@gmail.com>
2021-04-20 10:07:46 -0400

    Knight Special Move
    - Knight and archer special moves are completed

--- src/com/project/Render/KnightSpeicalMove.java
+++ src/com/project/Render/KnightSpeicalMove.java
@@ -2,15 +2,24 @@ package com.project.Render;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
 
 import com.project.BoardController.Location;
 import com.project.ChessPieces.IChessPiece;
 import com.project.ChessPieces.KnightPiece;
+import com.project.ChessPieces.ProbabilityController;
 import com.project.Main.Main;
+import com.project.TeamController.Team;
+import com.project.TeamController.TeamType;
 
+import java.awt.Color;
 import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
@@ -18,11 +27,12 @@ public class KnightSpeicalMove {
 
 	private JFrame frmKnightSpecialMove;
 	
-	private IChessPiece knight;
+	private IChessPiece piece;
+	
+	private boolean alreadyRender = false;
 	
 	public KnightSpeicalMove(IChessPiece knight) {
-		this.knight = knight;
-		
+		this.piece = knight;
 		initialize();
 		frmKnightSpecialMove.setVisible(true);
 	}
@@ -42,17 +52,14 @@ public class KnightSpeicalMove {
 		JButton yesButton = new JButton("Yes");
 		yesButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("Attempting special move");
-				
-				KnightPiece n = (KnightPiece)knight;
-				ArrayList<Location> attacks = n.getAttackLocations();
-				
-				for(Location a : attacks) {
+				if(!alreadyRender) {
+					System.out.println("Attempting special move");
 					
-					
+					KnightPiece n = (KnightPiece)piece;
+					ArrayList<Location> attacks = n.getAttackLocations();
+					renderSpecialMove(attacks);
 				}
-				
-				Main.getBoardController().resetKnightSpecialMoveGUI();
+				alreadyRender = true;
 			}
 		});
 		yesButton.setBounds(25, 122, 138, 45);
@@ -63,6 +70,8 @@ public class KnightSpeicalMove {
 			public void actionPerformed(ActionEvent e) {
 				frmKnightSpecialMove.dispose();
 				Main.getBoardController().resetKnightSpecialMoveGUI();
+				Main.getBoardController().getNextMoveRenderer().clearCurrentRender();
+				clearCurrentRender();
 			}
 		});
 		noButton.setBounds(204, 122, 138, 45);
@@ -71,4 +80,155 @@ public class KnightSpeicalMove {
 		
 		Main.getBoardController().setKnightSpecialMoveGUI(this);
 	}
+	
+	private ArrayList<NextMoveObject> labels;
+	
+	private void renderSpecialMove(ArrayList<Location> moves) {
+		labels = new ArrayList<NextMoveObject>();
+		CenterPointManager center = new CenterPointManager();
+		
+		// Only a single move
+		if(moves.size() == 1) {}
+		
+		for(Location location : moves) {
+			boolean isPieceRendered = false;
+			Location point = center.centerPointAlgorithm(location.getX(), location.getZ());
+			JLabel pieceLabel = null;
+			// Check if location has a piece at it currently
+			IChessPiece pieceAt = Main.getBoardController().getPieceAtLocation(location);
+			if(pieceAt != null) {
+				isPieceRendered = true;
+				pieceLabel = pieceAt.getTexture().getPieceLabel();
+				
+				Border border = BorderFactory.createLineBorder(Color.GREEN, 3);
+				
+				// ADD THE ROLL INFORMATION
+				
+				
+				ProbabilityController probability = new ProbabilityController();
+				int[] values = probability.getProbablity(pieceAt, piece);
+				
+				String text = "";
+				for(int x = 0; x != values.length; x++) {
+					if(x != 0) {
+						text = text + ", " + values[x];
+					}else {
+						text = text + values[x];
+					}
+				}
+				
+				pieceLabel.setBorder(border);
+				
+			}else {
+				pieceLabel = new JLabel("");
+				pieceLabel.setBounds(point.getX(), point.getZ(), center.getDimensionOfPiece(), center.getDimensionOfPiece());
+				
+				Border border = BorderFactory.createLineBorder(Color.BLUE, 3);
+				pieceLabel.setBorder(border);
+			}
+			Main.getBoardController().getBoardObject().getFrame().repaint();
+			
+			MouseAdapter mouseListener = new MouseAdapter() {
+			    public void mouseClicked(MouseEvent e) {
+			    	if(Main.getBoardController().hasGameEnded()) {
+			    		Main.getBoardController().getNextMoveRenderer().clearCurrentRender();
+			    		clearCurrentRender();
+			    		return;
+			    	}
+			    	
+			    	// Move the object to here
+			    	for(NextMoveObject obj : labels) {
+			    		if(obj.getLabel().equals(e.getComponent())) {
+			    			// Check if there is a piece there
+			    			Main.getBoardController().getNextMoveRenderer().clearCurrentRender();
+			    			IChessPiece piece1 = Main.getBoardController().getPieceAtLocation(obj.getLocation());
+			    			
+			    			// TODO ADD FUZZY LOGIC HERE!
+			    			if(piece1 != null) { // A piece is here
+			    				DiceRollerGUI gui = new DiceRollerGUI();
+			    				ProbabilityController prob = new ProbabilityController();
+			    				gui.getFrame().repaint();
+			    				int rand = prob.getRandomNumber();
+			    				if(rand != 1) {
+			    					rand = rand - 1;
+			    				}
+			    				
+			    				int[] valuesNeeded = prob.getProbablity(piece, piece1);
+			    				
+			    				String temp = "";
+			    				for(int x = 0; x != valuesNeeded.length; x++) {
+			    					temp = temp + " " + valuesNeeded[x];
+			    				}
+			    				System.out.println("Probabilty needed:" + temp);
+			    				
+			    				try {
+									Thread.sleep(2500);
+								} catch (InterruptedException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+			    				
+			    				boolean success = false;
+			    				for(int x = 0; x != valuesNeeded.length; x++) {
+			    					if(rand == valuesNeeded[x]) {
+			    						success = true;
+			    					}
+			    				}
+			    				
+			    				if(success) {
+			    					gui.getRollingLabel().setText(rand + " (Success!)");
+			    					
+			    					Main.getBoardController().movePieceOnBoard(piece, obj.getLocation());
+				    				Main.getBoardController().getNextMoveRenderer().clearCurrentRender();
+				    				clearCurrentRender();
+				    				
+				    				piece1.destroyPiece();
+			    					Main.getBoardController().checkForGameFinished();
+			    					
+			    				}else {
+			    					gui.getRollingLabel().setText(rand + " (Missed!)");
+			    					Main.getBoardController().getNextMoveRenderer().clearCurrentRender();
+			    					clearCurrentRender();
+			    					
+			    					frmKnightSpecialMove.dispose();
+			    					Main.getBoardController().resetKnightSpecialMoveGUI();
+			    				}
+			    				
+			    				gui.getFrame().repaint();
+			    				
+			    			}
+			    			
+			    			break;
+			    		}
+			    	}
+			    }
+			}; 
+			
+			pieceLabel.addMouseListener(mouseListener);
+			
+			labels.add(new NextMoveObject(piece, pieceLabel, location, isPieceRendered, mouseListener));
+			if(!isPieceRendered) {
+				Main.getBoardController().getBoardObject().getFrame().add(pieceLabel);
+			}
+			    Main.getBoardController().getBoardObject().getFrame().repaint();
+		}
+		
+	}
+	
+	// Remove all current renders
+	private void clearCurrentRender() {
+		for(NextMoveObject obj : labels) {
+			if(!obj.isPieceRendered()) {
+				obj.getLabel().setBorder(null);
+				Main.getBoardController().getBoardObject().getFrame().remove(obj.getLabel());
+			}else {
+				// It's a rendered piece, remove the border
+				obj.getLabel().setBorder(null);
+				obj.getLabel().removeMouseListener(obj.getMouseAdapter());
+			}
+		}
+		labels.clear();
+		Main.getBoardController().getBoardObject().getFrame().repaint();
+	}
+	
 }
